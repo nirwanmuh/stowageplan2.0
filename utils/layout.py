1ff82165893bf5712ff0779da55b8070b58ff71c
nirwanmuh <59735784+nirwanmuh@users.noreply.github.com>
2026-03-26 18:21:13 +0700

    Create layout.py

---
+++ utils/layout.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from utils.stowage import compute_cog
+
+def plot_layout(items, L, W):
+    fig, ax = plt.subplots(figsize=(10, 5))
+
+    ax.add_patch(
+        patches.Rectangle((-L/2, -W/2), L, W, fill=False, linewidth=2)
+    )
+
+    for v in items:
+        x, y = v["pos"]
+        ax.add_patch(
+            patches.Rectangle(
+                (x - v["length"]/2, y - v["width"]/2),
+                v["length"],
+                v["width"],
+                fill=False,
+                edgecolor="blue"
+            )
+        )
+        ax.text(x, y, v["name"], ha="center", fontsize=7)
+
+    cx, cy = compute_cog(items)
+    ax.scatter(cx, cy, color='red')
+    ax.text(cx, cy, "CoG", color="red")
+
+    ax.set_xlim(-L/2, L/2)
+    ax.set_ylim(-W/2, W/2)
+    ax.set_aspect("equal")
+
+    return fig
