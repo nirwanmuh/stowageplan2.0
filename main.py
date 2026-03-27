05d85158bb66af2d2df5af2de24638fff3112bca
nirwanmuh <59735784+nirwanmuh@users.noreply.github.com>
2026-03-27 14:45:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,41 @@
 import streamlit as st
 import json
-from utils.stowage import auto_arrange
+from utils.stowage import GA_arrange
 from utils.layout import plot_layout
 
 st.set_page_config(layout="wide")
-st.title("Stowage Plan Ferry – NO OVERLAP, AABB-BASED")
+st.title("Stowage Plan Ferry — Genetic Algorithm (GA) Version")
 
-L = st.number_input("Panjang kapal (m)", 40.0)
-W = st.number_input("Lebar kapal (m)", 12.0)
+L = st.number_input("Panjang kapal (meter)", 40.0)
+W = st.number_input("Lebar kapal (meter)", 12.0)
 
-empty_cog_x = st.number_input("CoG kapal X", 10.0)
-empty_cog_y = W/2
+cog_x = st.number_input("CoG Kapal X", 10.0)
+cog_y = W/2
 
 with open("data/vehicles.json") as f:
     VEHICLES = json.load(f)
 
-if "items" not in st.session_state:
-    st.session_state["items"] = []
+if "vehicles" not in st.session_state:
+    st.session_state["vehicles"] = []
 
-vehicle = st.selectbox("Pilih kendaraan", list(VEHICLES.keys()))
+vehicle = st.selectbox("Pilih Kendaraan", list(VEHICLES.keys()))
 
-if st.button("Tambahkan"):
+if st.button("Tambah"):
     v = VEHICLES[vehicle].copy()
     v["name"] = vehicle
-    st.session_state["items"].append(v)
+    st.session_state["vehicles"].append(v)
 
-    arranged = auto_arrange(
-        st.session_state["items"], L, W,
-        empty_cog_x, empty_cog_y
+if st.button("Generate GA Layout"):
+    st.session_state["vehicles"] = GA_arrange(
+        st.session_state["vehicles"],
+        L, W,
+        cog_x, cog_y
     )
 
-    st.session_state["items"] = arranged
-
-items = st.session_state["items"]
+items = st.session_state["vehicles"]
 
 if len(items) > 0:
-    fig = plot_layout(items, L, W, empty_cog_x, empty_cog_y)
+    fig = plot_layout(items, L, W, cog_x, cog_y)
     st.pyplot(fig, use_container_width=True)
+else:
+    st.write("Belum ada kendaraan.")
