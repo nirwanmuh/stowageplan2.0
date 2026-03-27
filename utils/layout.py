a05e33028472fad927596688e9cb8da65e3eddad
nirwanmuh <59735784+nirwanmuh@users.noreply.github.com>
2026-03-27 11:55:58 +0700

    Update layout.py

--- utils/layout.py
+++ utils/layout.py
@@ -19,37 +19,50 @@ COLOR_MAP = {
 def plot_layout(items, L, W, empty_cog_x, empty_cog_y):
 
     fig, ax = plt.subplots(figsize=(50, 14))
-
     ax.set_facecolor("#f0f0f0")
     fig.patch.set_facecolor("#111111")
 
+    # outline kapal
     ax.add_patch(
         patches.Rectangle((0,0), L, W, fill=False, linewidth=4, edgecolor="black")
     )
 
+    # garis CoG
     ax.axvline(empty_cog_x, color="blue", linestyle="--", linewidth=3)
     ax.axhline(empty_cog_y, color="blue", linestyle="--", linewidth=3)
 
     for v in items:
-        x = v["pos"][0] + L/2
-        y = v["pos"][1] + W/2
+        # center → visual
+        vx = v["pos"][0] + L/2
+        vy = v["pos"][1] + W/2
+
+        # CLAMP rectangle agar tidak terpotong
+        vx = max(v["length"]/2, min(L - v["length"]/2, vx))
+        vy = max(v["width"]/2,  min(W - v["width"]/2,  vy))
+
         c = COLOR_MAP.get(v["name"], "cyan")
 
         ax.add_patch(
             patches.Rectangle(
-                (x - v["length"]/2, y - v["width"]/2),
+                (vx - v["length"]/2, vy - v["width"]/2),
                 v["length"], v["width"],
                 fill=True, alpha=0.5,
                 edgecolor=c, facecolor=c, linewidth=3
             )
         )
 
-        ax.text(x, y, v["name"], fontsize=20, ha="center", color="black", weight="bold")
+        ax.text(vx, vy, v["name"],
+                fontsize=20, ha="center", color="black", weight="bold")
 
+    # COG kendaraan
     cx, cy = compute_cog(items)
-    ax.scatter(cx + L/2, cy + W/2, s=300, color="red")
-    ax.text(cx + L/2, cy + W/2, "CoG", fontsize=22, weight="bold", color="red")
+    cx_v = cx + L/2
+    cy_v = cy + W/2
 
+    ax.scatter(cx_v, cy_v, s=300, color="red")
+    ax.text(cx_v, cy_v, "CoG", fontsize=22, color="red", weight="bold")
+
+    # axis
     ax.set_xlim(0, L)
     ax.set_ylim(0, W)
     ax.set_aspect("equal")
