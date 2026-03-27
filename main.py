c5ab90a7c9e5266de467350369e46aebe8431d1e
nirwanmuh <59735784+nirwanmuh@users.noreply.github.com>
2026-03-27 14:48:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from utils.stowage import GA_arrange
 from utils.layout import plot_layout
 
 st.set_page_config(layout="wide")
-st.title("Stowage Plan Ferry — Genetic Algorithm (GA) Version")
+st.title("Ferry Stowage Plan — Genetic Algorithm")
 
-L = st.number_input("Panjang kapal (meter)", 40.0)
-W = st.number_input("Lebar kapal (meter)", 12.0)
+L = st.number_input("Panjang kapal", 40.0)
+W = st.number_input("Lebar kapal", 12.0)
 
 cog_x = st.number_input("CoG Kapal X", 10.0)
 cog_y = W/2
@@ -18,14 +18,14 @@ with open("data/vehicles.json") as f:
 if "vehicles" not in st.session_state:
     st.session_state["vehicles"] = []
 
-vehicle = st.selectbox("Pilih Kendaraan", list(VEHICLES.keys()))
+vehicle = st.selectbox("Pilih golongan kendaraan", list(VEHICLES.keys()))
 
 if st.button("Tambah"):
     v = VEHICLES[vehicle].copy()
     v["name"] = vehicle
     st.session_state["vehicles"].append(v)
 
-if st.button("Generate GA Layout"):
+if st.button("Generate GA"):
     st.session_state["vehicles"] = GA_arrange(
         st.session_state["vehicles"],
         L, W,
@@ -37,5 +37,3 @@ items = st.session_state["vehicles"]
 if len(items) > 0:
     fig = plot_layout(items, L, W, cog_x, cog_y)
     st.pyplot(fig, use_container_width=True)
-else:
-    st.write("Belum ada kendaraan.")
