909494d3ae011c585aa4b70a85eaebdea14e45a1
nirwanmuh <59735784+nirwanmuh@users.noreply.github.com>
2026-03-27 14:44:51 +0700

    Update layout.py

--- utils/layout.py
+++ utils/layout.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
-from utils.stowage import compute_cog, get_bbox
+from utils.stowage import get_bbox, compute_cog
 
 COLOR = {
     "Golongan I": "cyan",
@@ -16,32 +16,27 @@ COLOR = {
     "Golongan IX": "gold"
 }
 
-def plot_layout(items, ship_L, ship_W, cog_x, cog_y):
+def plot_layout(items, Ls, Ws, cog_x, cog_y):
 
-    fig, ax = plt.subplots(figsize=(50,14))
+    fig, ax = plt.subplots(figsize=(50, 14))
     ax.set_facecolor("#f0f0f0")
     fig.patch.set_facecolor("#111111")
 
-    # outline kapal
-    ax.add_patch(
-        patches.Rectangle((0,0), ship_L, ship_W, fill=False, linewidth=4, edgecolor="black")
-    )
+    # Ship outline
+    ax.add_patch(patches.Rectangle((0,0), Ls, Ws,
+                                   fill=False, linewidth=4, edgecolor='black'))
 
-    # garis CoG
     ax.axvline(cog_x, color="blue", linestyle="--", linewidth=3)
     ax.axhline(cog_y, color="blue", linestyle="--", linewidth=3)
 
-    # kendaraan
     for v in items:
         cx, cy = v["pos"]
-        vx = cx + ship_L/2
-        vy = cy + ship_W/2
+        bbox = get_bbox(cx, cy, v["length"], v["width"])
 
-        xmin, xmax, ymin, ymax = get_bbox(cx, cy, v["length"], v["width"])
-        xmin += ship_L/2
-        xmax += ship_L/2
-        ymin += ship_W/2
-        ymax += ship_W/2
+        xmin = bbox[0] + Ls/2
+        xmax = bbox[1] + Ls/2
+        ymin = bbox[2] + Ws/2
+        ymax = bbox[3] + Ws/2
 
         c = COLOR.get(v["name"], "cyan")
 
@@ -52,16 +47,18 @@ def plot_layout(items, ship_L, ship_W, cog_x, cog_y):
                 fill=True, edgecolor=c, facecolor=c, alpha=0.5, linewidth=3
             )
         )
+
         ax.text((xmin+xmax)/2, (ymin+ymax)/2,
-                v["name"], fontsize=20, ha="center", weight="bold")
+                v["name"], color="black", fontsize=18, weight="bold",
+                ha="center", va="center")
 
-    # CoG kendaraan
+    # Final CoG
     cx, cy = compute_cog(items)
-    ax.scatter(cx+ship_L/2, cy+ship_W/2, color="red", s=300)
-    ax.text(cx+ship_L/2, cy+ship_W/2, "CoG", fontsize=22, color="red")
+    ax.scatter(cx + Ls/2, cy + Ws/2, color="red", s=300)
+    ax.text(cx + Ls/2, cy + Ws/2, "CoG", fontsize=22, color="red")
 
-    ax.set_xlim(0, ship_L)
-    ax.set_ylim(0, ship_W)
+    ax.set_xlim(0, Ls)
+    ax.set_ylim(0, Ws)
     ax.set_aspect("equal")
 
     return fig
